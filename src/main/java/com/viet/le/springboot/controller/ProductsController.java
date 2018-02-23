2a275a234271733a2762b65f9537e2a67706dd32
viet <viettangle12@gmail.com>
2018-02-22 22:34:30 -0500

    added PUT

--- src/main/java/com/viet/le/springboot/controller/ProductsController.java
+++ src/main/java/com/viet/le/springboot/controller/ProductsController.java
@@ -2,6 +2,8 @@ package com.viet.le.springboot.controller;
 
 import com.viet.le.springboot.model.Product;
 import com.viet.le.springboot.repository.ProductRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 public class ProductsController {
 
     private ProductRepository productRepository;
+    private Logger LOG = LoggerFactory.getLogger(ProductsController.class);
 
     @Autowired
     public void productRepository(ProductRepository productRepository) {
@@ -29,4 +32,21 @@ public class ProductsController {
     public Product saveProduct(@RequestBody Product product) {
         return this.productRepository.save(product);
     }
+
+    @RequestMapping(path="{id}", method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Product updateProduct(@RequestBody Product productToUpdate, @PathVariable(name = "id") String id) {
+        Product foundProduct = this.productRepository.findOne(id);
+        if(foundProduct != null) {
+            foundProduct.setName(productToUpdate.getName());
+            foundProduct.setDescription(productToUpdate.getDescription());
+            foundProduct.setType(productToUpdate.getType());
+            foundProduct.setCategory(productToUpdate.getCategory());
+            return this.productRepository.save(foundProduct);
+        } else {
+            LOG.info("No products found with given id");
+            return productToUpdate;
+        }
+    }
+
+
 }
